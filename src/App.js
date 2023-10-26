91509285a5bc795900d3f16d7db588f1b1d61740
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-26 09:52:39 +0300

    yes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const toDo = {
+    userId = 1,
+    id: 1,
+    title: "delectus aut autem",
+    completed: false,
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <h1>
+      <Todo />
+    </h1>
+  )
 }
 
 export default App;
---
+++ src/ToDo.js
@@ -0,0 +1,12 @@
+
+function ToDo() {
+    return (
+    <>
+        <p>UserId: 1</p>
+        <p>Id: 1</p>
+        <p>delectus aut autem</p>
+        <input type="checkbox"></input>
+    </>
+    );
+}
+export default ToDo;
